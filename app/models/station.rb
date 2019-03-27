06ef47cf70f64b5e8d7f7670fe320754580f85a9
Scott Thomas <smthom05@gmail.com>
2019-03-27 11:17:19 -0600

    added other station attributes at least

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,8 +1,14 @@
 class Station
-  attr_reader :name
-  
+  attr_reader :name,
+              :address,
+              :fuel_type,
+              :distance
+
   def initialize(station_data)
     @name = station_data['station_name']
+    @address = station_data['street_address']
+    @fuel_type = station_data['feul_type_code']
+    @distance = station_data['distance']
   end
 
 end
