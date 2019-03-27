d8e2c20ee2eb0fffc771b3a44653236ce7836cc0
Scott Thomas <smthom05@gmail.com>
2019-03-27 11:19:16 -0600

    was adding those attributes to test

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -8,10 +8,13 @@ describe 'Station' do
     expect(station).to be_a(Station)
   end
 
-  it 'it has a name attribute' do
-    data = {"station_name" => 'station'} 
+  it 'it has a name, address, type, distance attributes' do
+    data = {"station_name" => 'station'}
     station = Station.new(data)
 
     expect(station.name).to eq("station")
+    # expect(station.address).to eq("station")
+    # expect(station.fuel_type).to eq("station")
+    # expect(station.distance).to eq("station")
   end
 end
